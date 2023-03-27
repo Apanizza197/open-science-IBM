2a511d46f0b7b906c5703feb57f9261e79384d6a
Agustín Panizza <agustinpanizza123@gmail.com>
2023-03-27 08:38:31 -0300

    Created custom ansatz

---
+++ ansatz.py
@@ -0,0 +1,51 @@
+from qiskit import QuantumCircuit, QuantumRegister
+from qiskit.circuit import Parameter
+
+def create_ansatz_1(num_qubits, layers = 1):
+    qc = QuantumCircuit(num_qubits)
+    for qubit in range(0, num_qubits, 2):
+        qc.h(qubit)
+        qc.cnot(qubit, qubit+1)
+
+    j = 0
+    for layer in range(layers):
+        for qubit in range(num_qubits):
+            qc.ry(Parameter('θ_' + str(j)), qubit)
+            j += 1
+        for qubit in range(num_qubits - 1):
+            qc.cnot(qubit, qubit+1)
+    return qc
+
+def create_ansatz_2(num_qubits, layers = 1):
+    qc = QuantumCircuit(num_qubits)
+    j = 0
+    for qubit in range(num_qubits):
+            qc.ry(Parameter('θ_' + str(j)), qubit)
+            j += 1
+    for layer in range(layers):
+        for qubit in range(num_qubits - 1):
+            qc.cnot(qubit, qubit+1)
+        for qubit in range(num_qubits):
+            qc.ry(Parameter('θ_' + str(j)), qubit)
+            j += 1
+       
+    return qc
+
+def create_ansatz_3(num_qubits, layers = 1):
+    qc = QuantumCircuit(num_qubits)
+    j = 0
+    for qubit in range(num_qubits):
+            qc.ry(Parameter('θ_' + str(j)), qubit)
+            j += 1
+    for layer in range(layers):
+        for qubit in range(0, num_qubits, 2):
+            qc.h(qubit)
+            qc.cnot(qubit, qubit+1)
+        for qubit in range(num_qubits):
+            qc.ry(Parameter('θ_' + str(j)), qubit)
+            j += 1
+       
+    return qc
+
+
+qc = create_ansatz_3(6, 2)
