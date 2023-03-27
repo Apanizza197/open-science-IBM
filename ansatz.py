62a8fd2b97a0debaaa3b2a99e1ee56c0a2ab54c0
Agustín Panizza <agustinpanizza123@gmail.com>
2023-03-27 12:12:06 -0300

    Created an example of lattice run and added a fourth ansatz

--- ansatz.py
+++ ansatz.py
@@ -3,7 +3,8 @@ from qiskit.circuit import Parameter
 
 def create_ansatz_1(num_qubits, layers = 1):
     qc = QuantumCircuit(num_qubits)
-    for qubit in range(0, num_qubits, 2):
+
+    for qubit in range(0, num_qubits - (num_qubits % 2), 2):
         qc.h(qubit)
         qc.cnot(qubit, qubit+1)
 
@@ -38,13 +39,25 @@ def create_ansatz_3(num_qubits, layers = 1):
             qc.ry(Parameter('θ_' + str(j)), qubit)
             j += 1
     for layer in range(layers):
-        for qubit in range(0, num_qubits, 2):
+        for qubit in range(0, (num_qubits % 2), 2):
             qc.h(qubit)
             qc.cnot(qubit, qubit+1)
         for qubit in range(num_qubits):
             qc.ry(Parameter('θ_' + str(j)), qubit)
             j += 1
        
+    return qc
+
+def create_ansatz_4(num_qubits, layers = 1):
+    qc = QuantumCircuit(num_qubits)
+    j = 0
+    for layer in range(layers):
+        for qubit in range(num_qubits):
+            qc.ry(Parameter('θ_' + str(j)), qubit)
+            j += 1
+        for qubit in range(num_qubits -1):
+            qc.cnot(qubit, qubit+1)
+        
     return qc
 
 
